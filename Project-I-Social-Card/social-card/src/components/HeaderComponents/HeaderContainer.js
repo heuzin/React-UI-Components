ad7f36b4ccc9a1d2ca31ba87305608ad486e8205
heuzin <matheusqs.arq@gmail.com>
2019-05-29 20:29:07 -0400

    Added content for the header and the card content

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+function Header() {
+    return (
+        <header className="header">
+            <ImageThumbnail />
+            <HeaderTitle />
+            <HeaderContent />
+        </header>
+    );
+}
+
+export default Header;
